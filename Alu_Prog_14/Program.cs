701b790ff0fcb566de2a5ab5f1e4cef87e0e52ea
Никита Василенко <ProMaren2017@yandex.ru>
2022-05-05 01:20:22 +0400

    Шляпа

--- Alu_Prog_14/Program.cs
+++ Alu_Prog_14/Program.cs
@@ -26,7 +26,7 @@ namespace Alu_Prog_14
 
         static void Game_Settings()
         {
-            Item.Add(new Inventory() { empty = true, count = 1, name = "шляпа" });
+            Item.Add(new Inventory() { empty = true, count = 1, name = "Шляпа" });
             Item.Add(new Inventory() { empty = true, count = 1, name = "камень" });
             Item.Add(new Inventory() { empty = true, count = 1, name = "меч" });
 
